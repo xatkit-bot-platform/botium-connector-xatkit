7df24987fffea961d7dc8e584d656f73d5758cc3
Hamza <hamza.eddouibi@gmail.com>
2020-09-04 14:54:59 +0200

    Add Botium metadata

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+const fs = require('fs')
+const path = require('path')
 const BotiumConnectorXatkit = require('./src/connector')
+
+const logo = fs.readFileSync(path.join(__dirname, 'logo.png')).toString('base64')
 
 module.exports = {
   PluginVersion: 1,
-  PluginClass: BotiumConnectorXatkit
+  PluginClass: BotiumConnectorXatkit,
+  PluginDesc: {
+    name: 'Botium Connector for Xatkit',
+    avatar: logo,
+    provider: 'Xatkit',
+    capabilities: [
+      {
+        name: 'XATKIT_SERVER_URL',
+        label: 'XATKIT_SERVER_URL',
+        description: 'Xatkit server url',
+        type: 'url',
+        required: true
+      }
+    ]
+  }
 }
